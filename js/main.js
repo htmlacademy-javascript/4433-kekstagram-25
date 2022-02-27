65ffc69fc3188462035c80ea84f2f7479f0df721
sukhoverkhova <sukhoverkhova.an@gmail.com>
2022-02-27 19:09:15 +0800

    Добавляет функции для генерации массива фотографий

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+const PHOTO_DESCRIPTIONS = [
+  'Сегодня прекрасный день! Приехала на дачу.',
+  'Меня трудно понять, невозможно сохранить и что-то там ещё.',
+  'Сколько людей, столько и мнений.',
+  'Никогда не говори «никогда»',
+  'Коты — это жидкость. Мой Пафнутий тому доказательство'
+];
+
+const USER_NAMES = [
+  'Екатерина',
+  'Ольга Сергеевна',
+  'Мария',
+  'Спаниель Петрович',
+  'Никонор Парамонович',
+  'Юлия',
+  'Александр Иванович',
+  'Дмитрий Сергеевич',
+];
+
+const COMMENT_TEXTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const PHOTO_COUNT = 25;
+
 function getRandomNumber(min, max) {
   if (min < 0 || max < 0) {
     return '⛔ Ошибка. Некорректный диапазон: числа должны быть положительными.';
@@ -19,5 +49,29 @@ function checkCommentLength(comment, maxLength) {
   return maxLength >= commentLength;
 }
 
-checkCommentLength('Комментарий', 140);
-getRandomNumber(0, 1);
+const getRandomArrayElement = (elements) => ( elements[getRandomNumber(0, elements.length - 1)] );
+
+const createComment = () => {
+  return {
+    id: getRandomNumber(1, 200),
+    avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
+    message: getRandomArrayElement(COMMENT_TEXTS),
+    name: getRandomArrayElement(USER_NAMES)
+  };
+};
+
+const createPhotoItem = (i) => {
+  return {
+    id: i,
+    url: 'photos/' + i + '.jpg',
+    description: getRandomArrayElement(PHOTO_DESCRIPTIONS),
+    likes: getRandomNumber(15, 200),
+    comments: Array.from({length: getRandomNumber(0, 10)}, createComment)
+  };
+};
+
+const photos = [];
+
+for (let i = 1; i <= PHOTO_COUNT; i++) {
+  photos.push(createPhotoItem(i));
+}
