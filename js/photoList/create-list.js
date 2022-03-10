91b3eb0a23f98d1c7f2947f29addea88278b979b
sukhoverkhova <sukhoverkhova.an@gmail.com>
2022-03-10 17:32:27 +0300

    Добавляет открытие попапа при нажатии на фото

--- js/photoList/create-list.js
+++ js/photoList/create-list.js
@@ -1,3 +1,7 @@
+import {createPhotoItem} from '../data.js';
+import consts from '../consts.js';
+import {openPopup} from '../photo/popup.js';
+
 const createPictureList = (container, template, pictures) => {
   const pictureFragment = document.createDocumentFragment();
 
@@ -8,11 +12,28 @@ const createPictureList = (container, template, pictures) => {
     pictureItem.querySelector('.picture__likes').textContent = photo.likes;
     pictureItem.querySelector('.picture__comments').textContent = photo.comments.length;
 
+    pictureItem.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      openPopup(photo.url, photo.likes, photo.comments, photo.description);
+    });
+
     pictureFragment.appendChild(pictureItem);
   });
 
   container.appendChild(pictureFragment);
 };
 
+const photos = [];
 
-export {createPictureList};
+for (let i = 1; i <= consts.PHOTO_COUNT; i++) {
+  photos.push(createPhotoItem(i));
+}
+
+const pictureTemplate = document
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const pictureListContainer = document.querySelector('.pictures');
+
+createPictureList(pictureListContainer, pictureTemplate, photos);
