caf8f30e5294b83e0cd0f859452002d990813360
sukhoverkhova <sukhoverkhova.an@gmail.com>
2022-03-10 14:56:26 +0300

    Добавляет отображение фотографий из случайного массива

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-import {createPhotoItem} from './data.js';
-import dataConsts from './consts.js';
-
-const photos = [];
-
-for (let i = 1; i <= dataConsts.PHOTO_COUNT; i++) {
-  photos.push(createPhotoItem(i));
-}
+import './photoList/photo-list.js';
--- js/photoList/photo-list.js
+++ js/photoList/photo-list.js
@@ -1,1 +1,28 @@
-// модуль для интерфейса показа списка фото
+import {createPhotoItem} from '../data.js';
+import consts from '../consts.js';
+
+const photos = [];
+
+for (let i = 1; i <= consts.PHOTO_COUNT; i++) {
+  photos.push(createPhotoItem(i));
+}
+
+const pictureTemplate = document
+  .querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const pictureList = document.querySelector('.pictures');
+const pictureFragment = document.createDocumentFragment();
+
+photos.forEach((photo) => {
+  const pictureItem = pictureTemplate.cloneNode(true);
+
+  pictureItem.querySelector('.picture__img').src = photo.url;
+  pictureItem.querySelector('.picture__likes').textContent = photo.likes;
+  pictureItem.querySelector('.picture__comments').textContent = photo.comments.length;
+
+  pictureFragment.appendChild(pictureItem);
+});
+
+pictureList.appendChild(pictureFragment);
