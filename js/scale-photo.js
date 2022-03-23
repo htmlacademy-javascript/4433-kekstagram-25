58156e675662c52cd05c4491e5739d05faa39331
sukhoverkhova <sukhoverkhova.an@gmail.com>
2022-03-23 22:37:53 +0300

    Добавляет эффекты для изображений

--- js/scale-photo.js
+++ js/scale-photo.js
@@ -1,32 +1,36 @@
 const SCALE_STEP = 25;
+const MAX_SCALE = 100;
+const MIN_SCALE = 25;
 
 const imagePreviewElement = document.querySelector('.img-upload__preview img');
 
-const rangeInput = document.querySelector('.scale__control--value');
-const rangeBiggerButton = document.querySelector('.scale__control--bigger');
-const rangeSmallerButton = document.querySelector('.scale__control--smaller');
+const scaleInput = document.querySelector('.scale__control--value');
+const scaleBiggerButton = document.querySelector('.scale__control--bigger');
+const scaleSmallerButton = document.querySelector('.scale__control--smaller');
 
 const scalePhotoPreview = (value) => {
   const scale = parseInt(value, 10) / 100;
   imagePreviewElement.style.transform = `scale(${scale})`;
 };
 
-scalePhotoPreview(rangeInput.value);
+scalePhotoPreview(scaleInput.value);
 
-rangeBiggerButton.addEventListener('click', () => {
-  rangeInput.value = parseInt(rangeInput.value, 10) + SCALE_STEP;
-  scalePhotoPreview(rangeInput.value);
+scaleBiggerButton.addEventListener('click', () => {
+  const newScale = parseInt(scaleInput.value, 10) + SCALE_STEP;
+
+  if (newScale <= MAX_SCALE) {
+    scalePhotoPreview(newScale);
+    scaleInput.value = `${newScale}%`;
+  }
 });
 
-rangeSmallerButton.addEventListener('click', () => {
-  rangeInput.value = parseInt(rangeInput.value, 10) - SCALE_STEP;
-  scalePhotoPreview(rangeInput.value);
+scaleSmallerButton.addEventListener('click', () => {
+  const newScale = parseInt(scaleInput.value, 10) - SCALE_STEP;
+
+  if (newScale >= MIN_SCALE) {
+    scalePhotoPreview(newScale);
+    scaleInput.value = `${newScale}%`;
+  }
 });
 
-rangeInput.addEventListener('change', () => {
-  scalePhotoPreview(rangeInput.value);
-});
-
-rangeInput.addEventListener('input', () => {
-  scalePhotoPreview(rangeInput.value);
-});
+export {scaleInput, scalePhotoPreview};
